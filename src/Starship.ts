5495c451a1923d2dc9b63ed7f37039d7da4a967f
eloisetaylor5693 <eloisetaylor5693@gmail.com>
2022-07-21 17:18:38 +0100

    formatting

--- src/Starship.ts
+++ src/Starship.ts
@@ -1,11 +1,11 @@
 export default interface Starship {
-    id: string;
-    name: string;
-    class: string;
-    maximumSpeed: string;
-    costInCredits: number;
-    passengerCapacity: number;
+  id: string;
+  name: string;
+  class: string;
+  maximumSpeed: string;
+  costInCredits: number;
+  passengerCapacity: number;
 
-    /** The total number of films featuring the Starship */
-    filmAppearances?: number
+  /** The total number of films featuring the Starship */
+  filmAppearances?: number;
 }
