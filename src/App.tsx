c1bfc928c1a7f479c0b6f7fc92a91b8e98bddede
eloisetaylor5693 <eloisetaylor5693@gmail.com>
2022-07-21 11:09:14 +0100

    moved logic for retrieving starships to app file

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,39 @@
-import Board from "./components/Board"
-import Card from "./components/Card"
+import { gql, useQuery } from "@apollo/client";
+import Board from "./components/Board";
+import Card from "./components/Card";
 
-/*
-  Connectr frontend boilerplate
-
-  Below is an potential starter to your app.
-  Please feel free to remove any components, code, libraries in this boilerplate if it doesn't work with your solution.
-
-  We use 'styled-components' at Connectr but please feel free to use whatever styling library or conventions you are more comfortable with.
-
-  Below is the criteria we will be looking at so please keep them in mind when implementing your solution.
-
-  - Coding style, i.e. your approach not whether you use semicolons or not ;)
-  - Usability. Although the design doesn’t necessarily need to be polished we expect the app to be usable without the user needing to read this document
-  - Your overall solution approach
-  - Maintainability and extensibility
-  - Use of responsive design
-  - Web Accessibility. Anyone should be able to use it.
-  - Meeting the requirements
-
-*/
+const GET_STARSHIPS = gql`
+  query root {
+    allStarships {
+      edges {
+        node {
+          id
+          starshipClass
+          name
+          passengers
+          maxAtmospheringSpeed
+        }
+      }
+    }
+  }
+`;
 
 function App() {
+  // getting all spaceships
+  const { loading, error, data } = useQuery(GET_STARSHIPS);
+
+  // error handling
+  if (loading) return <h1>'Loading...'</h1>;
+  if (error) return <p>`Error! ${error.message}`</p>;
+
+  // shuffle array of spaceships
+  // pop item from array
+
   return (
     <Board>
       <Card>Goodluck and have fun 👍</Card>
     </Board>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,43 +1,9 @@
 import styled from "styled-components";
-import { gql, useQuery } from "@apollo/client";
 
-const BoardContainer = styled.div`
+export default styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
   min-height: 100vh;
   background-image: url("../assets/stars.jpeg");
 `;
-
-const GET_STARSHIPS = gql`
-  query root {
-    allStarships {
-      edges {
-        node {
-          id,
-          starshipClass,
-          name,
-          passengers,
-          maxAtmospheringSpeed,
-        }
-      }
-    }
-  }
-`;
-
-const Board = (): JSX.Element => {
-  // getting all spaceships
-  const { loading, error, data } = useQuery(GET_STARSHIPS);
-
-  // error handling
-  if (loading) return <h1>'Loading...'</h1>;
-  if (error) return <p>`Error! ${error.message}`</p>;
-
-
-  // shuffle array of spaceships
-  // pop item from array
-  
-  return <BoardContainer></BoardContainer>;
-};
-
-export default Board;
