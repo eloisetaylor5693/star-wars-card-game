1191fec3ea51544d78897fcd5f093af34ac72cee
eloisetaylor5693 <eloisetaylor5693@gmail.com>
2022-07-21 17:43:46 +0100

    styling now accounts for new heading

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 export default styled.div`
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
   min-height: 100vh;
