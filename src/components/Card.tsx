ffb63b907a3b8edcdcffd6ca0295e267f01532e2
eloisetaylor5693 <eloisetaylor5693@gmail.com>
2022-07-21 11:41:11 +0100

    wired up rest of card stas and styled it

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -20,6 +20,22 @@ const StarShipStatsSection = styled.div`
   padding: 0.5rem 2rem;
   background-color: white;
   height: 100%;
+  div:first-child {
+    margin-top: 1.5rem;
+  }
+`;
+
+const StarShipStat = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 0.5rem 1rem;
+  background-color: #ffc625;
+  margin-bottom: 1.5rem;
+  border-radius: 0.5rem;
+`;
+
+const StarShipStatValue = styled.span`
+  text-align:end
 `;
 
 interface CardProps {
@@ -39,7 +55,20 @@ export const Card = ({ starship, ...props }: CardProps): JSX.Element => {
         width="320px"
         height="180px"
       />
-      <StarShipStatsSection></StarShipStatsSection>
+      <StarShipStatsSection>
+        <StarShipStat>
+          Max Speed: <StarShipStatValue>{starship.maximumSpeed}</StarShipStatValue>
+        </StarShipStat>
+        <StarShipStat>
+          Credit Cost: <StarShipStatValue>{starship.costInCredits}</StarShipStatValue>
+        </StarShipStat>
+        <StarShipStat>
+          Passengers: <StarShipStatValue>{starship.passengerCapacity}</StarShipStatValue>
+        </StarShipStat>
+        <StarShipStat>
+          Film Appearances: <StarShipStatValue>0</StarShipStatValue>
+        </StarShipStat>
+      </StarShipStatsSection>
     </CardContainer>
   );
 };
